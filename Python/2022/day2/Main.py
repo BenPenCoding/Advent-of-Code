ce8ad163dd6df5ca756b42c6ac6f7d26331bfa4f
Ben Pennycook <benpennycook@outlook.com>
2026-01-16 16:29:07 +0000

    Finished Main.py

--- Python/2022/day2/Main.py
+++ Python/2022/day2/Main.py
@@ -29,5 +29,36 @@ for line in file:
 
 print(f"Part 1: {totalScore}")
 
+file.close()
+
+winDict = {'A': 8, 'B': 9, 'C': 7}
+drawDict = {'A': 4, 'B': 5, 'C': 6}
+loseDict = {'A': 3, 'B': 1, 'C': 2}
+
+file = open("Python/2022/day2/puzzle.txt", "r")
+
+totalScore = 0
+
+for line in file:
+
+    theirMove = line[0]
+    myMove = line[2]
+
+    if myMove == 'X':
+
+        totalScore += loseDict[theirMove]
+        #print(loseDict[theirMove])
+
+    elif myMove == 'Y': 
+
+        totalScore += drawDict[theirMove]
+        #print(drawDict[theirMove])
+
+    else:
+
+        totalScore += winDict[theirMove]
+        #print(winDict[theirMove])
+
+print(f"Part 2: {totalScore}")    
 
     
